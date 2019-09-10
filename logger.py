5c92a43a296dafb0a3c6251b59dd24acc7b46b2b
Siddhesh Pisal <siddhesh.pisal@fractal.ai>
2019-09-10 14:08:34 +0530

    Create a Logger class with basic settings

---
+++ logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+
+class Logger(object):
+    LOGGING_DIR = "logs/"
+
+    def __init__(self, name):
+        name = name.replace('.log', '')
+        logger = logging.getLogger('logs.%s' % name)  # log_namespace can be replaced with your namespace
+        logger.setLevel(logging.DEBUG)
+        if not logger.handlers:
+            file_name = os.path.join(self.LOGGING_DIR, '%s.log' % name)
+            handler = logging.FileHandler(file_name)
+            formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s %(message)s')
+            handler.setFormatter(formatter)
+            handler.setLevel(logging.DEBUG)
+            logger.addHandler(handler)
+        self._logger = logger
+
+    def get(self):
+        return self._logger
