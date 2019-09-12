2ee420ce4071b479f1aea016e8f0f71aa08151d0
Siddhesh Pisal <siddhesh.pisal@fractal.ai>
2019-09-12 15:09:24 +0530

    Helper funcs for pandas

---
+++ helper/pandas.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from forecasting_engine.best_forecast import BestForecast
+
+
+def pandas_group_forecast(df, groupby_cols, y_col, pred_steps):
+    pass
+
+
+def group_forecast_apply(df):
+    pass
