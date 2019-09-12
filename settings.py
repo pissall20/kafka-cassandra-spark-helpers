1bc3f8984b9ade8eadaecf5d0bd46691b80b7ca2
Siddhesh Pisal <siddhesh.pisal@fractal.ai>
2019-09-12 14:45:08 +0530

    Updated settings to have time, value and id group

--- settings.py
+++ settings.py
@@ -8,8 +8,6 @@ CASSANDRA_TABLE_NAME = "demo_table"
 INITIAL_TRAINING_POINTS = 1500
 PREDICTION_STEPS_IN_SECONDS = 12
 
-TIME_COLUMN = "key"
-
 MODEL_LOCATION = "models/"
 MODEL_ARCHIVE = "archive/"
 
@@ -19,3 +17,7 @@ TABLE_SCHEMA = {
     "key": "timestamp",
     "value": "double",
 }
+
+TIME_COLUMN = "key"
+VALUE_COLUMN = "value"
+IDENTIFIER_GROUP = ["primary_id", "secondary_id"]
